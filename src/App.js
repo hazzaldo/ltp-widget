43b68209bcb4ad4332757dcfd8d01d614264c0e2
Hareth <hazzaldo@hotmail.com>
2022-08-11 23:50:45 +0100

    changes for 404 redirect

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Navigate from 'react-router-dom';
+import Route from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <Route path='/404' element={'<h1>Page cannot be found</h1>'} />
+        <Route path='*' element={<Navigate replace to='/404' />} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
