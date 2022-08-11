e12503fc00a01acb4237a712471d2b51148e180c
Hareth <hazzaldo@hotmail.com>
2022-08-11 23:56:45 +0100

    remove Router and Navigate

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Navigate from 'react-router-dom';
-import Route from 'react-router-dom';
+import { Navigate, Router } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <Route path='/404' element={'<h1>Page cannot be found</h1>'} />
-        <Route path='*' element={<Navigate replace to='/404' />} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
